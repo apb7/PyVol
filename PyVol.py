26aee32f2934485f974432d180cfd523dcf4c313
Apurv Bajaj <apurvabajaj007@gmail.com>
2017-07-19 23:13:28 +0530

    Create PyVol.py

---
+++ PyVol.py
@@ -0,0 +1,28 @@
+from sys import argv
+from subprocess import call
+
+def main():
+    if len(argv)==3:
+        while True:
+            volume=argv[2]
+            try:
+                volume=int(volume[:-1])
+                if volume>=0 and volume<=100:
+                    call(["amixer","-D","pulse","sset","Master",str(volume)+"%"])
+                else:
+                    print("Invalid Volume Value! Run Again!")
+                break
+
+            except ValueError:
+                print("Invalid Value! Run again!")
+                break
+    elif argv[1][2:]=="present":
+        call(["amixer","-D","pulse","get","Master"])
+    elif argv[1][2:]=="mute":
+        call(["amixer", "-D", "pulse", "sset", "Master", "0%"])
+    else:
+        print("Invalid Argument!")
+
+
+if __name__=="__main__":
+    main()
